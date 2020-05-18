2c5f357919c7fc586b02a9c9dfd762deea66f346
Saurabhdimri06 <dimrisaurabh06@gmail.com>
2020-05-18 20:29:05 +0530

    Footer for the dashboard

---
+++ restaurant_frontend/src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className="card text-center" style={{backgroundColor:'lightgrey'}}>
+                    <div className="card-header">
+                        <h4>“Pull up a chair. Take a taste. Come join us. Life is so endlessly delicious.”</h4>
+                        <h6>― Ruth Reichl</h6> 
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer
